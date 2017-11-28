2aedf83381477af074402cba82ea0dafda98f02b
harshh124 <harshh124@gmail.com>
2017-11-27 22:57:58 -0500

    Add files via upload

---
+++ Pre P2/P2/Updated Files with sort stuff/threads.h
@@ -0,0 +1,38 @@
+#include <pthread.h>
+
+struct file_dir{
+	char *wDir;
+	char *oDir;
+	char *sF;
+	char *curr_dir;
+	int compare;
+	struct mData *final;
+
+};
+struct wdir{
+	char wDir[200];
+};
+
+struct Counter {
+        int count;
+        pthread_mutex_t lock;
+};
+
+struct Sorter{
+	//struct
+	char *wDir;
+	char *oDir;
+	int k;
+	int comp_ptr;
+	pthread_mutex_t *lock;
+	struct mData *final;
+
+};
+
+
+void print_usage();
+void print_stat(struct file_dir *str);
+char* get_cwd(char* var);
+void print_cwd(char* var);
+int check_oDir(char* output); // 0 = Exists 1 = Does not exist 3 = Couldn't create
+int check_wDir(char* output); // 0 = Exists 1 = Does not exist
